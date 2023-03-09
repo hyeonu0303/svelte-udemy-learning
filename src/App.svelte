6b5e246f7a01a784f0a5bb3c270bad7b22aec0a4
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-03-09 23:41:30 +0900

    Section 4

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
   import ContactCard from "./ContactCard.svelte";
 
-  let name = "Max";
-  let title = "";
-  let image = "";
-  let description = "";
+  let name = "Hyeonwoo";
+  let title = "pro";
+  let image = "dd";
+  let description = "ddd";
 	let formState = '';
 
   let createContact = [];
@@ -38,10 +38,12 @@
   #form {
     width: 30rem;
     max-width: 100%;
+    margin:1rem 0;
   }
+
 </style>
 
-<div id="form">
+<form id="form">
   <div class="form-control">
     <label for="userName">User Name</label>
     <input type="text" bind:value={name} id="userName" />
@@ -58,13 +60,15 @@
     <label for="desc">Description</label>
     <textarea rows="3" bind:value={description} id="desc" />
   </div>
-</div>
+  <button on:click|preventDefault={addConstract} type="submit">Add Contact card</button>
+</form>
 
 <button on:click={addConstract}>카드추가</button>
 <button on:click={deleteFirst}>첫번째 카드제거</button>
 <button on:click={deleteLast}>마지막 카드제거</button>
 {#if formState === 'invalid'}
   <p>데이터를 다 입력후 눌러주세요!!</p>
+
 {:else}
   <p>데이터를 입력후 눌러주세요</p>
 {/if}
